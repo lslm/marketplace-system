833bcee0c4a4b0d49e5d6c874165a42e569adc58
Lucas Santos <lucas.santos.silva2011@gmail.com>
2022-06-03 21:19:23 -0300

    Created customers API

---
+++ customers-api/src/test/java/com/lslm/customersapi/CustomersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lslm.customersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
