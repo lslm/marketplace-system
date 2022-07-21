632b899782108e576a536dcb3cf3f8eb112e9bd7
Lucas S <lucas.santos.silva2011@gmail.com>
2022-07-21 20:05:06 -0300

    Remove unused imports

--- orders-api/src/main/java/com/lslm/ordersapi/controllers/OrderController.java
+++ orders-api/src/main/java/com/lslm/ordersapi/controllers/OrderController.java
@@ -2,18 +2,14 @@ package com.lslm.ordersapi.controllers;
 
 import com.lslm.ordersapi.clients.StockClient;
 import com.lslm.ordersapi.entities.Order;
-import com.lslm.ordersapi.entities.ProductStock;
 import com.lslm.ordersapi.producers.OrderProducer;
 import com.lslm.ordersapi.services.OrderService;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
